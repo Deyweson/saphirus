d6b51d2e64bb7612107fc756856790a146e1d061
Deyweson <deywesonalm@gmail.com>
2025-01-04 23:23:08 -0300

    :sparkles: Feat: Adiciona validação de código de produto antes de adicionar ao banco de dados

--- src/main/services/3-Produtos/add-product.ts
+++ src/main/services/3-Produtos/add-product.ts
@@ -5,6 +5,18 @@ export async function AddProduct(
   product: ProductModel
 ): Promise<{ success: boolean; message: string }> {
   try {
+    // validate prod code before add into database
+    const codeValidate = await db<ProductModel>('products')
+      .select('*')
+      .where('code', product.code)
+      .first()
+    if (codeValidate) {
+      return {
+        success: false,
+        message: 'O código do produto já esta cadastrado'
+      }
+    }
+
     await db<ProductModel>('products').insert(product)
     return {
       success: true,
